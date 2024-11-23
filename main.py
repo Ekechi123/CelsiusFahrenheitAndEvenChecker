2dc18012aaea9e2fdd3c6a570cd40e62574b5345
Ekechi123 <your_email@example.com>
2024-11-22 22:40:41 -0500

    Document Fahrenheit to Celsius conversion function

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
 def fahrenheit_to_celsius(fahrenheit):
+    """
+    Converts a temperature from Fahrenheit to Celsius.
+
+    Parameters:
+        fahrenheit (float): Temperature in Fahrenheit.
+
+    Returns:
+        float: Temperature in Celsius.
+    """
     return (fahrenheit - 32) * 5/9
 
