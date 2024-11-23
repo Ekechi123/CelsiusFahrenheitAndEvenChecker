34a31667947e4159163c4b8aadf8971e3ad11cd5
Ekechi123 <your_email@example.com>
2024-11-22 22:29:04 -0500

    Fix spacing in test_functions.py for readability

--- test_functions.py
+++ test_functions.py
@@ -5,7 +5,6 @@ def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(100) == 212
     assert celsius_to_fahrenheit(-40) == -40
 
-
 def test_fahrenheit_to_celsius():
     from main import fahrenheit_to_celsius
     assert fahrenheit_to_celsius(32) == 0
